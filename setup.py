50811c9a80d6c75a18d60ccbd753d867554ad18e
Segun <tundeoyegunsen@gmail.com>
2024-11-24 20:47:24 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mypack',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='EDSA example python package',
+    long_description=open('README.md').read(),
+    install_requires=['numpy', 'pandas'],
+    url='https://github.com/<username>/<package-name>',
+    author='<Your Name>',
+    author_email='<Your Email>'
+)
